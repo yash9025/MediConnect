9dc4bc9f06c11f194beee46f95caa61b4846232d
Yash Agrawal <agrawalyash994@gmail.com>
2025-03-09 19:25:46 +0530

    bcrypt to bcryptjs

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,6 @@
 //controlller functions for apis
 import doctorModel from "../models/doctorModel.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import appointmentModel from "../models/appointmentModel.js";
 
