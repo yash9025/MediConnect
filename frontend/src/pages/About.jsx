659ea9008e3388c78ef44c380ef6e93a498bf297
Yash Agrawal <agrawalyash994@gmail.com>
2025-09-19 23:58:35 +0530

    fixed backend

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { assets } from '../assets/assets';
 
 const About = () => {
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import  { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets.js'
 import axios from 'axios';
