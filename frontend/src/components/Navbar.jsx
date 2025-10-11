36d9d7cdd19d56f9e3f26c86398f3829cc1c22d8
Yash Agrawal <agrawalyash994@gmail.com>
2025-10-11 20:06:36 +0530

    updated admin url

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -84,7 +84,7 @@ const Navbar = () => {
                 </li>
                 <li>
                     <a
-                        href="https://mediconnect-admin-chem.onrender.com"
+                        href="https://mediconnect-admin-wlzf.onrender.com"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="bg-green-600 text-white px-4 py-2 rounded-full text-sm hover:bg-green-700 transition-all duration-200"
