eabdcfe64f61456cdcde765a983a3cb33ebdae13
Yash Agrawal <agrawalyash994@gmail.com>
2026-01-17 12:40:01 +0530

    added one field tokenNumber(BookingId)

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -1,19 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const appointmentSchema = new mongoose.Schema({
-    userId:{type:String , required:true},
+const appointmentSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
     docId: { type: String, required: true },
     slotDate: { type: String, required: true },
     slotTime: { type: String, required: true },
+    tokenNumber: { type: Number, required: true },
+    status: { type: String, default: "Pending" },
     userData: { type: Object, required: true },
     docData: { type: Object, required: true },
     amount: { type: Number, required: true },
-    date: { type: Number, required:true},
+    date: { type: Number, required: true },
     cancelled: { type: Boolean, default: false },
     payment: { type: Boolean, default: false },
     isCompleted: { type: Boolean, default: false }
-},  { timestamps: true });
+  },
+  { timestamps: true }
+);
 
-const appointmentModel = mongoose.models.appointment || mongoose.model('appointment' , appointmentSchema);
+const appointmentModel =
+  mongoose.models.appointment ||
+  mongoose.model("appointment", appointmentSchema);
 
 export default appointmentModel;
