108d3dea676ec685a79348b93f46ca8625544011
Yash Agrawal <agrawalyash994@gmail.com>
2026-01-17 20:04:01 +0530

    user authentication before blood report analysis

--- backend/middlewares/authUser.js
+++ backend/middlewares/authUser.js
@@ -22,7 +22,7 @@ const authUser = async (req, res, next) => {
         }
 
         const token_decode = jwt.verify(token, process.env.JWT_SECRET);
-        req.body.userId = token_decode.id; 
+        req.userId = token_decode.id; 
         next();
 
     } catch (error) {
