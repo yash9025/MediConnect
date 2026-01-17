abe034da4ca8521b4b1a88ff416036da86a1267d
Yash Agrawal <agrawalyash994@gmail.com>
2026-01-17 20:04:46 +0530

    added new route delete history

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getChatHistory, saveChatMessage } from '../controllers/chatController.js';
+import { getChatHistory, saveChatMessage, deleteChatHistory } from '../controllers/chatController.js';
 import authUser from '../middlewares/authUser.js';
 
 const chatRouter = express.Router();
@@ -11,5 +11,9 @@ chatRouter.post('/get', authUser, getChatHistory);
 // Route to save a new message
 // Matches Frontend: axios.post('/api/chat/save', ...)
 chatRouter.post('/save', authUser, saveChatMessage);
+
+// Route to delete/reset chat history
+// Matches Frontend: axios.delete('/api/chat/delete', ...)
+chatRouter.delete('/delete', authUser, deleteChatHistory);
 
 export default chatRouter;
